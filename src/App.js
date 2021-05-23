1a9426ed1dda826776b07206f3dabda26c409c83
Arunkumar-RR <rrarunkumardgct@gmail.com>
2021-05-23 12:53:30 +0530

    updated no do available component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import No_data_available from './components/no_data_available/No_data_available'
 
 function App() {
   return (
     <div className="App">
-     <h1>project setup</h1>
+      <No_data_available/>
     </div>
   );
 }
