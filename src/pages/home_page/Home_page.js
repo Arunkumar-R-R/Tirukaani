4ca9e12aab6bc33e82759fdef0d69019526512f6
Arunkumar-RR <rrarunkumardgct@gmail.com>
2021-05-30 09:53:32 +0530

    created home-page

--- src/pages/home_page/Home_page.js
+++ src/pages/home_page/Home_page.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Button from '../../components/button/Button';
 import No_data_available from './../../components/no_data_available/No_data_available'
+import './Home_page.css'
 export default function Home_page()
 {
     return (
         <>
           <No_data_available/>
-          <Button></Button>
+          <div className='button-sticky-button'>
+            <Button className='add-client-btn'></Button>
+          </div>
         </>
       );
 }
