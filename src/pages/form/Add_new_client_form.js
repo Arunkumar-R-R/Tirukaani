9587d8a5860b2acbab99809e02d7d2633911daa8
Arunkumar-R-R <rrarunkumardgct@gmail.com>
2021-06-15 07:55:27 +0530

    removed border around close-btn

--- src/pages/form/Add_new_client_form.js
+++ src/pages/form/Add_new_client_form.js
@@ -2,6 +2,13 @@ import React,{useEffect} from 'react';
 import Add_new_client from '../../components/client/add_new_client';
 import Button from '../../components/button/Button';
 import './form.css';
+let obj={
+    name:'',
+    silvertype:'',
+    weight:'',
+    touch:'',
+    labourtouch:'',
+};
 
 export default function Add_new_client_form(props)
 {
@@ -13,21 +20,16 @@ export default function Add_new_client_form(props)
         let weight = document.querySelector('#weight').value;
         let touch = document.querySelector('#touch').value;
         let labourtouch = document.querySelector('#labourTouch').value;
-        let obj={
-            name:'',
-            silvertype:'',
-            weight:'',
-            touch:'',
-            labourtouch:'',
-
-        };
+       
         obj.name = name;
         obj.silvertype = silvertype;
         obj.weight = weight;
         obj.touch = touch;
         obj.labourtouch = labourtouch;
-        console.log(obj);
+        // props.clientsetter([props.clientarray,obj]);
+        props.clientsetter.concat(obj);
         props.closemodal();
+        // console.log(props.clientarray);
         }
     
     return(
