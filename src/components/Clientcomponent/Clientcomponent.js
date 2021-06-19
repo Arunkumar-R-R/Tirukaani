3862c0e8bc363ecd1ce9644bbdcb005ff560e52d
Arunkumar-R-R <rrarunkumardgct@gmail.com>
2021-06-19 06:44:10 +0530

    added key to client component created

--- src/components/Clientcomponent/Clientcomponent.js
+++ src/components/Clientcomponent/Clientcomponent.js
@@ -7,8 +7,7 @@ export default function Clientcomponent({clients})
     {
         console.log(e.target.textContent);
     }
-    return clients.map((client)=>{
-        return <div className='client'onClick={opendealpage}>{client.name}</div>
-        console.log(client)
+    return clients.map((client,index)=>{
+        return <div key={index} className='client'onClick={opendealpage}>{client.name}</div>
     });
 }
