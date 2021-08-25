f44324a8a16140e7419db68be1bb8ee427d1294e
Arunkumar-R-R <rrarunkumardgct@gmail.com>
2021-08-25 10:44:00 +0530

    added  handlesubmit method

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,18 +1,36 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Button from '../../components/Button/Button'
 import Toast from '../../components/Toast/Toast';
+import { useAuth } from '../../Context/AuthProvider';
 import './Login.css'
 
 
-export default function Login({getuserdata})
+export default function Login()
 {
+
+    const history = useHistory();
+    const { login } = useAuth();
+    const [error, setError] = useState("")
+
+     async function handlesubmit(e){
+        e.preventDefault();
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
+        try {
+            setError("")
+            await login(email, password);
+            history.push("/home");
+        } catch {
+            setError("Failed to log in");
+          }
+    } 
 
     return(
         <div class="container ">
             <div class="row mx-auto height-full">
-              
                 <div class="col-12 cl-xl-12  col-lg-12 col-md-12 col-sm-12 col-xs-12 m-auto">
-                    <form className='form' onSubmit={getuserdata}>
+                    <form className='form' onSubmit={handlesubmit}>
                             <h3 className='form_title'>Login</h3>
                             <div className='form_element'>
                                 <label >Email</label>
@@ -24,6 +42,7 @@ export default function Login({getuserdata})
                             </div>  
                             <Button type={'submit'} text={"Login"} buttontype={'primarybtn'} />
                     </form>
+                    {error && <Toast type='alert-danger' text={error}></Toast>}
                 </div>
             </div>
         </div>
