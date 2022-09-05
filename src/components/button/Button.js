99a6bfa2f9d2daa1c3df15082544a80d5563362e
arunkumar-r-r <rrarunkumardgct@gmail.com>
2022-09-05 14:27:40 +0530

    Updated Button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './Button.css'
-export default function Button(props)
-{
-    return(
-        <button type={props.type} onClick={props.onClick} className={`button ${props.buttontype}`}>
-            {props.isLoading ? 'Loading... ': props.text}
-        </button>
-    );
+import React from "react";
+import "./Button.css";
+export default function Button(props) {
+  return (
+    <button
+      type={props.type}
+      onClick={props.onClick}
+      className={`button ${props.buttontype}`}
+    >
+      {props.isLoading ? "Loading... " : props.text}
+    </button>
+  );
 }
